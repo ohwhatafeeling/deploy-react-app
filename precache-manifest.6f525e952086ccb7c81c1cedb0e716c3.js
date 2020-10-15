9cdd91510d4235108042475d91f6f6ee678970fb
ohwhatafeeling <pyro_michael@hotmail.com>
2020-10-15 12:54:27 +0930

    Updates

---
+++ precache-manifest.6f525e952086ccb7c81c1cedb0e716c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd69b35cb3572701f49d",
+    "url": "/deploy-react-app/static/css/main.02ceaf56.chunk.css"
+  },
+  {
+    "revision": "fd69b35cb3572701f49d",
+    "url": "/deploy-react-app/static/js/main.fd69b35c.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/deploy-react-app/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "b0ac1d20de17d82cde8c",
+    "url": "/deploy-react-app/static/js/runtime~main.b0ac1d20.js"
+  },
+  {
+    "revision": "9732002292a24a9314f8f642fec30c27",
+    "url": "/deploy-react-app/index.html"
+  }
+];
